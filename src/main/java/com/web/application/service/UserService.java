d2f0a8468583f18459061c7d1778c0455aea3082
daddy <losertime7@gmail.com>
2018-08-21 20:04:51 +0800

    Bug fix

---
+++ src/main/java/com/web/application/service/UserService.java
@@ -0,0 +1,4 @@
+package com.web.application.service;
+
+public interface UserService {
+}
--- src/test/java/com/web/application/ApplicationTests.java
+++ src/test/java/com/web/application/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.web.application;
 
+import com.web.application.domain.User;
 import com.web.application.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,4 +19,13 @@ public class ApplicationTests {
     public void selById() {
         userMapper.selectByPrimaryKey(1);
     }
+
+    @Test
+    public void Login() {
+        User user = new User();
+        user.setUsername("毛老师");
+        user.setPassword("1001010");
+        userMapper.selectByNameAndPassword(user);
+    }
+
 }
